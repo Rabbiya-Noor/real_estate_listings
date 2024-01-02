a7c635a672a26cb5b0d9acf3bf1306f2d0ac5513
ir-minahil <mmustafa@myintellirent.com>
2024-01-02 19:23:32 +0500

    making apps for difference group of modals

--- real_estate_listing_project/settings.py
+++ real_estate_listing_project/settings.py
@@ -36,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
+
+INSTALLED_APPS += ['agents', 'applicants', 'properties']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
